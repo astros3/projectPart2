043ccde79d0c62265ad5d2e1facefeb7ee18b397
Mann Patel <mannjpatel234@gmail.com>
2026-03-09 19:06:15 -0600

    name change

--- app/src/main/java/com/example/projectpart_3/MainActivity.java
+++ app/src/main/java/com/example/projectpart_3/MainActivity.java
@@ -3,10 +3,10 @@ package com.example.projectpart_3;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity_mann extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_mann);
+        setContentView(R.layout.activity_main);
     }
 }
